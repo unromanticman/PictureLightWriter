588d608d2bee5cc8ee0e0b976c5e5de7d7218417
unromanticman <umworksite@gmail.com>
2015-12-19 11:47:51 +0800

    Patch By UM

---
+++ PictureLight/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace PictureLight
+{
+    partial class PictureLight
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PictureLight));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.TagPic = new System.Windows.Forms.ImageList(this.components);
+            this.listviewData = new System.Windows.Forms.ListView();
+            this.imageListData = new System.Windows.Forms.ImageList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.ContentBox = new System.Windows.Forms.RichTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.edttxt = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 40);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(111, 449);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseClick);
+            // 
+            // TagPic
+            // 
+            this.TagPic.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("TagPic.ImageStream")));
+            this.TagPic.TransparentColor = System.Drawing.Color.Transparent;
+            this.TagPic.Images.SetKeyName(0, "01.PNG");
+            // 
+            // listviewData
+            // 
+            this.listviewData.Location = new System.Drawing.Point(141, 40);
+            this.listviewData.Name = "listviewData";
+            this.listviewData.Size = new System.Drawing.Size(634, 231);
+            this.listviewData.TabIndex = 1;
+            this.listviewData.UseCompatibleStateImageBehavior = false;
+            this.listviewData.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listviewData_MouseClick);
+            // 
+            // imageListData
+            // 
+            this.imageListData.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit;
+            this.imageListData.ImageSize = new System.Drawing.Size(100, 100);
+            this.imageListData.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "個人分類";
+            // 
+            // ContentBox
+            // 
+            this.ContentBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ContentBox.Location = new System.Drawing.Point(141, 308);
+            this.ContentBox.Name = "ContentBox";
+            this.ContentBox.Size = new System.Drawing.Size(634, 143);
+            this.ContentBox.TabIndex = 3;
+            this.ContentBox.Text = "";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(703, 458);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(72, 31);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // edttxt
+            // 
+            this.edttxt.AutoSize = true;
+            this.edttxt.Location = new System.Drawing.Point(140, 284);
+            this.edttxt.Name = "edttxt";
+            this.edttxt.Size = new System.Drawing.Size(32, 12);
+            this.edttxt.TabIndex = 5;
+            this.edttxt.Text = "編輯:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 492);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(787, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(76, 17);
+            this.toolStripStatusLabel1.Text = "狀態:無動作";
+            // 
+            // PictureLight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 514);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.edttxt);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.ContentBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listviewData);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PictureLight";
+            this.Text = "PictureLight";
+            this.Load += new System.EventHandler(this.PictureLight_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList TagPic;
+        private System.Windows.Forms.ListView listviewData;
+        private System.Windows.Forms.ImageList imageListData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox ContentBox;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label edttxt;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+
+    }
+}
+
---
+++ PictureLight/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace PictureLight
+{
+    public partial class PictureLight : Form
+    {
+
+        public string selectTagName;
+        public string fileName;
+
+        public ImageList showImageListData;
+   
+        //初始化資料夾
+        void initizeTag()
+        {
+            string path = System.Environment.CurrentDirectory + "\\PictureLightFile\\Tag\\";
+            string[] dirs = Directory.GetDirectories(path);/*目錄(含路徑)的陣列*/
+            ArrayList dirlist = new System.Collections.ArrayList();/*用來儲存只有目錄名的集合*/
+            foreach (string item in dirs)
+            {
+                dirlist.Add(Path.GetFileNameWithoutExtension(item));//走訪每個元素只取得目錄名稱(不含路徑)並加入dirlist集合中
+            }
+            foreach (var item in dirlist)
+            {//顯示目錄名稱(不含路徑)
+                ListViewItem items = new ListViewItem(item.ToString());
+                items.ImageIndex = 0;
+                listView1.Items.AddRange(new ListViewItem[] { items });
+            }
+
+        }
+
+
+        public PictureLight()
+        {
+            InitializeComponent();
+      
+        }
+
+        private void PictureLight_Load(object sender, EventArgs e)
+        {
+            this.listView1.LargeImageList = TagPic;
+            initizeTag();
+            //開啟時修正xml錯誤檔案
+            XMLRemove();
+        }
+        private void AddImageToImageList(ImageList iml, Bitmap bm, string key, float wid, float hgt)
+        {
+            // Make the bitmap.
+            Bitmap iml_bm = new Bitmap(
+            iml.ImageSize.Width,
+            iml.ImageSize.Height);
+            using (Graphics gr = Graphics.FromImage(iml_bm))
+            {
+                gr.Clear(Color.Transparent);
+                gr.InterpolationMode = InterpolationMode.High;
+                // See where we need to draw the image to scale it properly.
+                RectangleF source_rect = new RectangleF(
+                0, 0, bm.Width, bm.Height);
+                RectangleF dest_rect = new RectangleF(
+                0, 0, iml_bm.Width, iml_bm.Height);
+                dest_rect = ScaleRect(source_rect, dest_rect);
+                // Draw the image.
+                gr.DrawImage(bm, dest_rect, source_rect,
+                GraphicsUnit.Pixel);
+            }
+            // Add the image to the ImageList.
+            iml.Images.Add(key, iml_bm);
+        }
+        private RectangleF ScaleRect(RectangleF source_rect, RectangleF dest_rect)
+        {
+            float source_aspect =
+            source_rect.Width / source_rect.Height;
+            float wid = dest_rect.Width;
+            float hgt = dest_rect.Height;
+            float dest_aspect = wid / hgt;
+            if (source_aspect > dest_aspect)
+            {
+                // The source is relatively short and wide.
+                // Use all of the available width.
+                hgt = wid / source_aspect;
+            }
+            else
+            {
+                // The source is relatively tall and thin.
+                // Use all of the available height.
+                wid = hgt * source_aspect;
+            }
+            // Center it.
+            float x = dest_rect.Left + (dest_rect.Width - wid) / 2;
+            float y = dest_rect.Top + (dest_rect.Height - hgt) / 2;
+            return new RectangleF(x, y, wid, hgt);
+        }
+
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            listviewData.Items.Clear();
+            selectTagName = ((ListView)sender).FocusedItem.Text.ToString();
+            changeToShowPicture(selectTagName);
+        }
+        public void changeToShowPicture(string tagName)
+        {
+
+            showImageListData = new ImageList();
+            this.listviewData.LargeImageList = showImageListData;
+            showImageListData.ImageSize = new Size(100, 100);
+            showImageListData.ColorDepth = ColorDepth.Depth16Bit;
+
+            //資料夾名稱+完整路徑
+            string path = System.Environment.CurrentDirectory + "\\PictureLightFile\\Tag\\" + tagName + "\\";
+            //取得裡面所有圖片暫存到imageList
+            foreach (string url in System.IO.Directory.GetFileSystemEntries(path))
+            {
+                string fileName = url.Replace(path, "");
+                // Get the image.
+                Bitmap bm = new Bitmap(url);
+
+                float source_aspect = bm.Width / (float)bm.Height;
+                // Make the large image.
+                AddImageToImageList(showImageListData,
+                bm, fileName,
+                showImageListData.ImageSize.Width,
+                showImageListData.ImageSize.Height);
+            }
+
+            //加入到List裡面顯示出來
+            for (int i = 0; i < showImageListData.Images.Keys.Count; i++)
+            {
+                ListViewItem items = new ListViewItem(showImageListData.Images.Keys[i].ToString());
+                items.ImageIndex = i;
+                listviewData.Items.AddRange(new ListViewItem[] { items });
+            }
+            GC.Collect();
+        }
+
+
+        string XMLGet(string tag, string name)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(".\\PictureLightFile\\Config.xml");
+                XmlNode root = doc.SelectSingleNode("/PictureLight/Picture");
+                foreach (XmlElement elm in root)
+                {
+                    if (elm.GetAttribute("Tag") == tag && elm.GetAttribute("FileName") == name)
+                    {
+                        return elm.GetAttribute("FileContent");
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return "";
+        }
+
+        void XMLInsert(string tag, string name, string content)
+        {
+            if (XMLGet(tag, name) == "")
+            {
+                //並沒有此筆資料，所以用插入
+                XmlDocument doc = new XmlDocument();
+                doc.Load(".\\PictureLightFile\\Config.xml");
+                XmlNode root = doc.SelectSingleNode("/PictureLight/Picture");
+                XmlElement info = doc.CreateElement("Information");
+                info.SetAttribute("Tag", tag);
+                info.SetAttribute("FileName", name);
+                info.SetAttribute("FileContent", content);
+                root.AppendChild(info);
+                doc.Save(".\\PictureLightFile\\Config.xml");
+            }
+            else if (XMLGet(tag, name) != "")
+            {
+                //找到，此筆資料，使用修改
+                XmlDocument doc = new XmlDocument();
+                doc.Load(".\\PictureLightFile\\Config.xml");
+                XmlNode root = doc.SelectSingleNode("/PictureLight/Picture");
+                foreach (XmlElement elm in root)
+                {
+                    if (elm.GetAttribute("Tag") == tag && elm.GetAttribute("FileName") == name)
+                    {
+                        elm.SetAttribute("FileContent", content);
+                    }
+                }
+                doc.Save(".\\PictureLightFile\\Config.xml");
+            }
+
+        }
+
+        void XMLRemove()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(".\\PictureLightFile\\Config.xml");
+            XmlNodeList root = doc.SelectSingleNode("/PictureLight/Picture").ChildNodes;
+
+            for (int j = 0; j <= root.Count;j++ )
+            {
+                foreach (XmlNode xn in root)
+                {
+                    XmlElement xe = (XmlElement)xn;
+                    int isExist = 0;
+                    for (int i = 0; i < listView1.Items.Count; i++)
+                    {
+                        if (xe.GetAttribute("Tag") == listView1.Items[i].Text)
+                        {
+                            isExist = 1;
+                        }
+                    }
+                    if (isExist == 0)
+                    {
+                        doc.SelectSingleNode("/PictureLight/Picture").RemoveChild(xe);
+
+                    }
+                }
+            }
+
+
+            doc.Save(".\\PictureLightFile\\Config.xml");
+        }
+
+        private void listviewData_MouseClick(object sender, MouseEventArgs e)
+        {
+            fileName = ((ListView)sender).FocusedItem.Text.ToString();
+            edttxt.Text = "正在編輯: " + selectTagName + " ->" + fileName;
+            toolStripStatusLabel1.Text = "正在編輯: "+selectTagName+" ->" + fileName;
+            toolStripStatusLabel1.ForeColor = Color.Green;
+            ContentBox.Text = XMLGet(selectTagName, fileName);
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            XMLInsert(selectTagName, fileName, ContentBox.Text);
+            toolStripStatusLabel1.Text =DateTime.Now.ToString("HH:mm:ss")+ "成功修改:" + fileName ;
+            toolStripStatusLabel1.ForeColor = Color.Red;
+        }
+
+    }
+
+
+}
